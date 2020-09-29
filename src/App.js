616e1f397fd2fc09f4e0ce71cc4559bccb641653
Zionknight-crypto <info.davidhauser@gmail.com>
2020-09-29 08:25:36 +0300

    bug fixes

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>Monsters Rolodex</h1>
+        <h1 className="page-title">Monsters Rolodex</h1>
         <SearchBox 
         placeholder="search box"
           handleChange={this.onSearchChange} />
